a1d516994fa45dcd259917eee609cf11d7cb2bca
Regina Timirgaleeva <sadikovaregina@mail.ru>
2023-06-10 11:58:30 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         BonusService service = new BonusService();
-
+        long expected = 30;
         long amount = 1000_60;
         boolean registered = true;
 
-        service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
+
+        boolean passed = expected == actual;
+
+        System.out.print(passed);
     }
 }
 
